9ff264b8d3d49f86138107f3fb7d3d08fcd80720
deidg <64797316+deidg@users.noreply.github.com>
2023-02-02 10:16:51 +0300

    Initial Commit

---
+++ Vlad_pattern State/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vlad_pattern State
+//
+//  Created by Alex on 02.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
