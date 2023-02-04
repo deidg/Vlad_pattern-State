d51d88d6a2947e800e2290956df103b2d4c866d6
deidg <64797316+deidg@users.noreply.github.com>
2023-02-04 14:22:35 +0300

    checking connection Xcode -GiHub

--- Vlad_pattern State/File.swift
+++ Vlad_pattern State/File.swift
@@ -14,12 +14,60 @@ verride func viewDidLoad() {
     textField.delegate = self
 }
 
-let reverser = MyReverser()
 
-le
-    let textToReverse: String = textField.text ?? ""
-    
-    func reverse(textToReverse: String){ // -> String {
-        let reversedText =  reverser.reverseTextFunc(textToReverse: textToReverse)
-        label.text = reversedText
-        appState = .result
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        //     view.endEditing(true)
+//        button.backgroundColor = UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1)
+//        appState = .fill
+//    }
+//
+//}
+
+
+
+extension ViewController {
+
+enum StateEnum {   // множество состояний для построения приложения через паттер State
+    case start
+    case fill //(text: String)
+    case result//(result: String)
+}
+}
+
+extension ViewController: UITextFieldDelegate {
+//отслеживает изменение текста в моменте
+func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    if let text = textField.text,
+       let textRange = Range(range, in: text) {
+        let updatedText = text.replacingCharacters(in: textRange, with: string)
+        if updatedText == "" {
+            appState = .start
+            
+        } else {
+            //                divider.backgroundColor = UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1)
+            appState = .fill // (text: updatedText)
+            
+        }
+    }
+    return true
+}
+}
+
+
+/*
+
+func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) { //-> Bool {
+if let text = textField.text,
+let textRange = Range(range, in: text) {
+let updatedText = text.replacingCharacters(in: textRange, with: string)
+}
+
+}
+
+
+
+*/
+
+
+
+// начать с обдумывания экранов
